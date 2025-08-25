dc6d00142cef3527502baa26b7d68a9a320a09e9
Yuri David Silva Duarte <102871804+Boyuridod@users.noreply.github.com>
2025-08-25 12:02:45 -0300

    Create 3.ForOf.js

---
+++ Conteudos/3.ForOf.js
@@ -0,0 +1,39 @@
+const estadosBrasileiros = [
+    "Acre",
+    "Alagoas",
+    "Amapá",
+    "Amazonas",
+    "Bahia",
+    "Ceará",
+    "Distrito Federal",
+    "Espírito Santo",
+    "Goiás",
+    "Maranhão",
+    "Mato Grosso",
+    "Mato Grosso do Sul",
+    "Minas Gerais",
+    "Pará",
+    "Paraíba",
+    "Paraná",
+    "Pernambuco",
+    "Piauí",
+    "Rio de Janeiro",
+    "Rio Grande do Norte",
+    "Rio Grande do Sul",
+    "Rondônia",
+    "Roraima",
+    "Santa Catarina",
+    "São Paulo",
+    "Sergipe",
+    "Tocantins"
+];
+
+for (const estado of estadosBrasileiros) {
+    console.log(estado + " é um estado brasileiro")
+}
+
+const nome = "Yuri Duarte"
+
+for(let caractere of nome){
+    console.log(caractere.toUpperCase())
+}
