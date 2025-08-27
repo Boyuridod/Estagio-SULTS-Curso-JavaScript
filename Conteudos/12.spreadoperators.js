4f74d1e44bb52072a0627e25be3d7412fe3eb674
Yuri David Silva Duarte <102871804+Boyuridod@users.noreply.github.com>
2025-08-27 19:09:37 -0300

    Feat: 12 spreead operators

---
+++ Conteudos/12.spreadoperators.js
@@ -0,0 +1,12 @@
+lista1 = [1, 3, 5, 7, 9];
+lista2 = [2, 4, 6, 8, 10];
+
+listaTotal = [...lista1, ...lista2]; // Ele espalha "spread" os numeros na total
+
+console.log(listaTotal);
+
+const objeto = {a: 1, b:"a"};
+
+const objetoCopia = {...objeto} // Diferente de const objetoCopia = objeto
+
+console.log(objeto, objetoCopia, objeto === objetoCopia)
