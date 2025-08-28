b7fe89ab6c0cfc2abff9e6a65b960d561f175945
Yuri David Silva Duarte <102871804+Boyuridod@users.noreply.github.com>
2025-08-28 16:01:39 -0300

    Update 18.memoization.js

--- Conteudos/18.memoization.js
+++ Conteudos/18.memoization.js
@@ -4,20 +4,27 @@ function fibonacci(n, memoria){
     if(memoria[n]){
         return memoria[n];
     }
-    if(n == 0){
+
+    else if(n == 0){
         memoria[n] = 0;
-        return memoria[n]
     }
-    if(n == 1){
+
+    else if(n == 1){
         memoria[n] = 1
-        return memoria[n];
     }
-    memoria[n] = fibonacci((n - 1), memoria) + fibonacci((n - 2), memoria);
+
+    else{
+        memoria[n] = fibonacci((n - 1), memoria) + fibonacci((n - 2), memoria);
+    }
+
     return memoria[n];
 }
 
 for(let i = 0; i < 10; i++){
     console.log(fibonacci(i, memoria));
 }
+
+console.log(fibonacci(100, memoria));
+console.log(fibonacci(50, memoria));
 
 console.log(memoria)
